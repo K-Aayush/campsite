5d8d5382ff380602a0a2f257523ad07cd7edc8a1
Aayush <ayushkarkee6@gmail.com>
2025-05-26 11:17:55 +0545

    changed the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import AuthProvider from "@/components/commons/AuthProvider";
 import { Toaster } from "sonner";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,8 +18,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Mayur Wellness",
-  description: "Mayur Wellness, campsite",
+  title: "Hash One Campsite",
+  description: "Where Adventure, Nature and Well-being Come Together",
   icons: {
     icon: "/last-logo.png",
   },
@@ -30,18 +31,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          <header>
-            <Toaster />
-            <Navbar />
-          </header>
-          <main>{children}</main>
-          <Footer />
-        </AuthProvider>
+        <Providers>
+          <AuthProvider>
+            <header>
+              <Toaster />
+              <Navbar />
+            </header>
+            <main>{children}</main>
+            <Footer />
+          </AuthProvider>
+        </Providers>
       </body>
     </html>
   );
