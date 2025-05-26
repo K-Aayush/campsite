d5461791e6f4331ed2392a1c548d8171912af2cb
Aayush <ayushkarkee6@gmail.com>
2025-05-26 12:19:36 +0545

    udated theme for hero and subhero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -57,8 +57,8 @@ export default function Hero() {
 
       {/* Background decorative elements */}
       <div className="absolute top-0 left-0 w-full h-full overflow-hidden pointer-events-none">
-        <div className="absolute top-0 left-0 w-1/3 h-1/3 bg-primary-tint/20 rounded-full blur-3xl transform -translate-x-1/2 -translate-y-1/2" />
-        <div className="absolute bottom-0 right-0 w-1/2 h-1/2 bg-primary-tint/10 rounded-full blur-3xl transform translate-x-1/4 translate-y-1/4" />
+        <div className="absolute top-0 left-0 w-1/4 h-1/4 bg-primary-tint/15 rounded-full blur-4xl transform -translate-x-1/3 -translate-y-1/3" />
+        <div className="absolute bottom-0 right-0 w-1/3 h-1/3 bg-primary-tint/8 rounded-full blur-4xl transform translate-x-1/3 translate-y-1/3" />
       </div>
     </div>
   );
