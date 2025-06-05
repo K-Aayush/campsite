c2f27c7d032fe145d41b9099f87845d968d6f98f
Aayush <ayushkarkee6@gmail.com>
2025-06-05 12:24:25 +0545

    updated service

--- src/app/admin/service/page.tsx
+++ src/app/admin/service/page.tsx
@@ -7,7 +7,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { showToast } from "@/utils/Toast";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-export default function AdminServicesPage() {
+export default function AdminServicesPage() { 
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
