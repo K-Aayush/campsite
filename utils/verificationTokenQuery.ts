95ea33edb86d036170328f4d843f55792cb16345
Aayush <ayushkarkee6@gmail.com>
2025-06-10 12:53:45 +0545

    fixed issue

--- utils/verificationTokenQuery.ts
+++ utils/verificationTokenQuery.ts
@@ -2,7 +2,7 @@ import { db } from "./db";
 
 export const getVerificationTokenByToken = async (token: string) => {
   try {
-    const verificationToken = await db.verificationtoken.findUnique({
+    const verificationToken = await db.verificationToken.findUnique({
       where: { token },
     });
 
@@ -14,7 +14,7 @@ export const getVerificationTokenByToken = async (token: string) => {
 
 export const getVerificationTokenByEmail = async (email: string) => {
   try {
-    const verificationToken = await db.verificationtoken.findFirst({
+    const verificationToken = await db.verificationToken.findFirst({
       where: { email },
     });
 
